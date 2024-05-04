a2611c0e47d7d85b110a55e8aee5d21872a15bfd
Joy <mbuvimumbuambua99@gmail.com>
2024-05-04 14:12:41 +0300

    complete lab

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+
+function Article({title,date,preview}){
+
+return(
+    <div>
+    <h3>{title}</h3>
+    <h4>{date}</h4>
+    <p>{preview}</p>
+
+
+
+    </div>
+)}
+
+export default Article
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header ({blogData}){
+return (
+    <div>
+        <h1>{blogData.name}</h1>
+    </div>
+)
+
+}
+export default Header
